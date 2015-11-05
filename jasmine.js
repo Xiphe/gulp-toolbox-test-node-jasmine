5089dc092ad3bed105c41582dd749a2ae4b3113b
Xiphe <github@xiphe.net>
2015-11-05 02:23:33 +0100

    refactor(jsamineCli): relax a bit

--- jasmine.js
+++ jasmine.js
@@ -1,9 +1,8 @@
 'use strict';
 
-const CONFIG_PARAMETER_POSITION = 2;
 var Jasmine = require('jasmine');
 var jasmine = new Jasmine();
 
-jasmine.loadConfig(JSON.parse(process.argv[CONFIG_PARAMETER_POSITION]));
+jasmine.loadConfig(JSON.parse(process.argv[2]));
 
 jasmine.execute();
