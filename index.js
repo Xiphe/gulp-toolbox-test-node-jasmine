61c9970127432227a9df816bdcca195ebd1cc2cb
Xiphe <github@xiphe.net>
2015-11-17 11:10:54 +0100

    feat: adjust to current toolbox api, request config

--- index.js
+++ index.js
@@ -4,16 +4,21 @@ var spawn = require('child_process').spawn;
 var path = require('path');
 var jasmineCli = path.join(__dirname, 'jasmine.js');
 
-module.exports = function testNodeJasmineFactory() {
-  function testNodeJasmine() {
-    var config = {
-      'spec_dir': 'test',
-      'spec_files': [
-        '**/*Spec.js'
-      ],
-      'helpers': []
-    };
+module.exports = function testNodeJasmineFactory(requestConfig) {
+  var config = requestConfig({
+    'files.test.node.specs': {
+      as: 'spec_files',
+      default: ['test/node/**/*Spec.js']
+    },
+    'files.test.node.helpers': {
+      as: 'helpers',
+      default: ['']
+    }
+  });
 
+  config['spec_dir'] = '';
+
+  function testNodeJasmine() {
     return spawn('node', [jasmineCli, JSON.stringify(config)], {
       stdio: 'inherit'
     });
